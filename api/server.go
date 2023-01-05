0004f85773d43adb9b8533f27a7c3f7cda77e476
suedoh <suedoh36@proton.me>
2023-01-05 01:20:27 -0500

    Added product handler and two methods interfaces get and insert

--- api/server.go
+++ api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+    "encoding/json"
     "net/http"
 
     "github.com/suedoh/go-commerce/types"
@@ -8,8 +9,34 @@ import (
     "github.com/anthdm/weavebox"
 )
 
+type CreateProductRequest struct {
+   SKU string `json:"sku"`
+   Name string `json:"name"`
+}
+
+type ProductStorer interface {
+   Insert(*types.Product) error 
+   GetByID(string) (*types.Product, error)
+}
+
 type ProductHandler struct {
-    
+   store ProductStorer 
+}
+
+func NewProductHandler(pStore ProductStorer) *ProductHandler {
+    return &ProductHandler{
+        store: pStore,
+    } 
+}
+
+
+func (h *ProductHandler) HandlePostProduct(c *weavebox.Context) error {
+	productReq := CreateProductRequest{}
+	if err := json.NewDecoder(c.Request().Body).Decode(productReq); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, productReq)
 }
 
 func (h *ProductHandler) HandleGetProduct(c *weavebox.Context)  error {
