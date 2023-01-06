4d70a76218eaa14f8cb492219b5a45f960f827f9
suedoh <suedoh36@proton.me>
2023-01-06 00:48:48 -0500

    added mongo client and product handler

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+    "context"
+
     "github.com/suedoh/go-commerce/api"
+    "github.com/suedoh/go-commerce/store"
 
     "github.com/anthdm/weavebox"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main()  {
-   app := weavebox.New()
-   adminRoute := app.Box("/admin")
-
-   productHandler := &api.ProductHandler{}
+    app := weavebox.New()
+    adminRoute := app.Box("/admin")
 
 
-   adminRoute.Get("/product", productHandler.HandleGetProduct)
-   adminRoute.Post("/product", productHandler.HandlePostProduct)
 
-   app.Serve(3001)
+    client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+    if err != nil {
+        panic(err)
+    }
+
+
+    productStore := store.NewMongoProductStore(client)
+    productHandler := api.NewProductHandler(productStore)
+
+
+    adminRoute.Get("/product", productHandler.HandleGetProduct)
+    adminRoute.Post("/product", productHandler.HandlePostProduct)
+
+    app.Serve(3001)
 }
