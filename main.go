df0a498d6adaed520aa39228edd046f12847b85c
suedoh <suedoh36@proton.me>
2023-01-06 22:40:02 -0500

    Wired up mongo and added getByID route with slug

--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-    "context"
+	"context"
+	"fmt"
 
-    "github.com/suedoh/go-commerce/api"
-    "github.com/suedoh/go-commerce/store"
+	"github.com/suedoh/go-commerce/api"
+	"github.com/suedoh/go-commerce/store"
 
-    "github.com/anthdm/weavebox"
+	"github.com/anthdm/weavebox"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func handleAPIError(ctx *weavebox.Context, err error)  {
+   fmt.Println(err) 
+}
 
 func main()  {
     app := weavebox.New()
+    app.ErrorHandler = handleAPIError
     adminRoute := app.Box("/admin")
 
-
-
-    client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+    client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://0.0.0.0:27017"))
     if err != nil {
         panic(err)
     }
 
-
-    productStore := store.NewMongoProductStore(client)
+    productStore := store.NewMongoProductStore(client.Database("go-commerce"))
     productHandler := api.NewProductHandler(productStore)
 
 
-    adminRoute.Get("/product", productHandler.HandleGetProduct)
+    adminRoute.Get("/product/:id", productHandler.HandleGetProductByID)
     adminRoute.Post("/product", productHandler.HandlePostProduct)
 
     app.Serve(3001)
